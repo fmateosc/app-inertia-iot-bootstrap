cb2e6d8c0c462cc9ce6f4c3ef105b3815fe18a46
fernando rrrrr <fmateosc@gmail.com>
2022-03-03 18:50:22 +0000

    Proyecto con bootstrap y fontawesome

---
+++ resources/views/app.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title inertia>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700" rel="stylesheet">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}">
+
+    <!-- Scripts -->
+    @routes
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.26.0/moment.min.js"></script>
+    <script src="{{ mix('js/app.js') }}" defer></script>
+</head>
+
+<body class="bg-light">
+    @inertia
+
+    @env ('local')
+    <script src="http://localhost:3000/browser-sync/browser-sync-client.js"></script>
+    @endenv
+</body>
+
+</html>
